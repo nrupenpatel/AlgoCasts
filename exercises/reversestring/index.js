f3e6ef4d076a23f79632e862b5de329c2094a764
nrupenpatel <105888203+nrupenpatel@users.noreply.github.com>
2022-09-09 16:01:23 -0700

    Reverse a String in JS

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,51 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+/*
+    Split the string which converts to an character array
+    apply the reverse function on the character array
+    and once it reverses then apply  the join function and deliemiter is '' and return the 
+*/
+function reverse_1(str) {
+  return str.split("").reverse().join("");
+}
+/*
+    Simple basic exeuction
+    Traverse from end of string one character at a time and add to a variable string
 
-module.exports = reverse;
+*/
+function reverse_2(str) {
+  let rev_str = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    rev_str += str.charAt(i);
+  }
+  return rev_str;
+}
+
+function reverse_3(str) {
+  let reversed = "";
+  for (let character of str) {
+    reversed = character + reversed;
+  }
+  return reversed;
+}
+
+function reverse_4(str) {
+  return str.split("").reduce((reve, char) => char + reve, "");
+}
+
+/*
+  
+  */
+/*
+  
+  */
+
+// 4 different solutios of reverse a string
+
+module.exports = {
+  reverse_1,
+  reverse_2,
+  reverse_3,
+  reverse_4,
+};
--- exercises/reversestring/test.js
+++ exercises/reversestring/test.js
@@ -1,13 +1,49 @@
-const reverse = require('./index');
+const reverse = require("./index");
 
-test('Reverse function exists', () => {
-  expect(reverse).toBeDefined();
+test("Reverse1 function exists", () => {
+  expect(reverse.reverse_1("abcd")).toBeDefined();
 });
 
-test('Reverse reverses a string', () => {
-  expect(reverse('abcd')).toEqual('dcba');
+test("Reverse1 reverses a string", () => {
+  expect(reverse.reverse_1("abcd")).toEqual("dcba");
 });
 
-test('Reverse reverses a string', () => {
-  expect(reverse('  abcd')).toEqual('dcba  ');
+test("Reverse1 reverses a string", () => {
+  expect(reverse.reverse_1("  abcd")).toEqual("dcba  ");
+});
+
+test("Reverse2 function exists", () => {
+  expect(reverse.reverse_2("abcd")).toBeDefined();
+});
+
+test("Reverse2 reverses a string", () => {
+  expect(reverse.reverse_2("abcd")).toEqual("dcba");
+});
+
+test("Reverse2 reverses a string", () => {
+  expect(reverse.reverse_2("  abcd")).toEqual("dcba  ");
+});
+
+test("Reverse3 function exists", () => {
+  expect(reverse.reverse_3("abcd")).toBeDefined();
+});
+
+test("Reverse3 reverses a string", () => {
+  expect(reverse.reverse_3("abcd")).toEqual("dcba");
+});
+
+test("Reverse3 reverses a string", () => {
+  expect(reverse.reverse_3("  abcd")).toEqual("dcba  ");
+});
+
+test("Reverse4 function exists", () => {
+  expect(reverse.reverse_4("abcd")).toBeDefined();
+});
+
+test("Reverse4 reverses a string", () => {
+  expect(reverse.reverse_4("abcd")).toEqual("dcba");
+});
+
+test("Reverse4 reverses a string", () => {
+  expect(reverse.reverse_4("  abcd")).toEqual("dcba  ");
 });
