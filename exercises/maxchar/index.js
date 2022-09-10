23cf277a726e8752b36be1a05726b3598c1fd566
nrupenpatel <105888203+nrupenpatel@users.noreply.github.com>
2022-09-10 11:36:13 -0700

    max char in a string & reverse Integer problem
    max char in a string & reverse Integer problem

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -4,7 +4,49 @@
 // --- Examples
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
+/*
+function maxChar(str) {
+  let map_ = new Map();
+  for (let char of str.split("")) {
+    if (map_.has(char)) {
+      map_.set(char, map_.get(char) + 1);
+    } else {
+      map_.set(char, 1);
+    }
+  }
+  console.log(map_);
+  let max_value = 0;
+  let maChar = "";
+  for (let char of str.split("")) {
+    if (map_.get(char) > max_value) {
+      max_value = map_.get(char);
+      maChar = char;
+    }
+  }
+  return maChar;
+}
+*/
 
-function maxChar(str) {}
+function maxChar(str) {
+  const chars = {};
+  for (let char of str) {
+    if (!chars[char]) {
+      chars[char] = 1;
+    } else {
+      chars[char]++;
+    }
+  }
+  console.log(chars);
+  let max_val = 0;
+  let max_char = "";
+  for (let [key, value] of Object.entries(chars)) {
+    if (value > max_val) {
+      max_val = value;
+      max_char = key;
+    }
+  }
+  return max_char;
+}
 
+console.log(maxChar("abcccccccd"));
 module.exports = maxChar;
--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -7,7 +7,25 @@
 //   reverseInt(500) === 5
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
-
-function reverseInt(n) {}
+/*
+function reverseInt(n) {
+  let str = n.toString();
+  if (Math.sign(n) == -1 && str.endsWith("0")) {
+    str = str.substr(1, str.lastIndexOf("0") - 1);
+  } else if (str.startsWith("-")) {
+    str = str.substr(1, str.length - 1);
+  }
+  str = str.split("").reverse().join("");
+  if (n.toString().startsWith("-")) return parseInt(`-${str}`);
+  else return parseInt(`${str}`);
+}
+*/
+function reverseInt(n) {
+  let str = n.toString();
+  if (Math.sign(n) == -1)
+    return parseInt(str.split("").reverse().join("")) * Math.sign(n);
+  else return parseInt(str.split("").reverse().join(""));
+}
+console.log(reverseInt(-9891000));
 
 module.exports = reverseInt;
