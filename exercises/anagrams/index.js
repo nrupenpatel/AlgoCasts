b31a8b0b82ba43e247be73ac2bd9c142648b7761
nrupenpatel <105888203+nrupenpatel@users.noreply.github.com>
2022-09-10 13:43:56 -0700

    more commits

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,58 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  let isAnagram = true;
+  stringA = stringA
+    .replace(/\s/g, "")
+    .replace(/[^a-zA-Z0-9]/g, "")
+    .toLowerCase();
+  stringB = stringB
+    .replace(/\s/g, "")
+    .replace(/[^a-zA-Z0-9]/g, "")
+    .toLowerCase();
+  if (stringA.length !== stringB.length) return !isAnagram;
+  else {
+    let chars = {};
+    for (let charA of stringA) {
+      if (!chars[charA]) chars[charA] = 1;
+      else chars[charA]++;
+    }
+    for (let charB of stringB) {
+      if (!chars[charB]) chars[charB] = 1;
+      else chars[charB]--;
+    }
+    for (let val of Object.values(chars)) {
+      if (val !== 0) {
+        isAnagram = false;
+        break;
+      }
+    }
+    return isAnagram;
+  }
+}
+
+function anagrams_(stringA, stringB) {
+  let isAnagram = true;
+  stringA = stringA
+    .replace(/\s/g, "")
+    .replace(/[^a-zA-Z0-9]/g, "")
+    .toLowerCase();
+  stringB = stringB
+    .replace(/\s/g, "")
+    .replace(/[^a-zA-Z0-9]/g, "")
+    .toLowerCase();
+  if (stringA.length !== stringB.length) return !isAnagram;
+  else {
+    if (
+      stringA.split("").sort().join("").toString() !==
+      stringB.split("").sort().join("").toString()
+    )
+      isAnagram = false;
+  }
+  return isAnagram;
+}
+
+console.log(anagrams_("RAIL! SAFETY!", "fairy tales"));
 
 module.exports = anagrams;
--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,25 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+//let ar_ = [];
+
+//ar_.push(new Array(1, 2));
+//console.log(ar_);
+
+function chunk(array, size) {
+  let ar_ = [];
+  let arraySize = array.length;
+  while (size < arraySize) {
+    //ar_.push(new Array(array.slice(0, size)));
+    ar_.push(array.slice(0, size));
+    array.splice(0, size);
+    arraySize = arraySize - size;
+  }
+  //if (arraySize !== 0) ar_.push(new Array(array.slice(0, arraySize)));
+  if (arraySize !== 0) ar_.push(array.slice(0, arraySize));
+  return ar_;
+}
+
+console.log(chunk([1, 2, 3, 4, 5], 0));
 
 module.exports = chunk;
