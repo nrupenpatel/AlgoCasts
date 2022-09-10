efcd71f7799fb7198fd6d8248944b926d55389f6
nrupenpatel <105888203+nrupenpatel@users.noreply.github.com>
2022-09-10 15:02:36 -0700

    capitalize problem

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,22 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+  // default ds ['    ', '    ', '    ', '    ']
+  // n = 4
+  //
+  let ds = Array(n).fill("####");
+  for (let i = 0; i < ds.length; i++) {
+    let str = ds[i];
+    for (let j = i + 1; j < n; j++) {
+      console.log(str);
+      str = str.replace(str.charAt(j), " ");
+      console.log(str);
+    }
+    ds[i] = str;
+    //console.log(ds[i]);
+  }
+}
+steps(4);
 
 module.exports = steps;
